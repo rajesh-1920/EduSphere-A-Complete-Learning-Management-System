cb4feb675cc004d1b347f7b047c4a4f80014be03
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-03-09 22:45:40 +0600

    project start

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>
+        <h3 class="heading"> Hi.<br>
+            Welcome to my project.
+        </h3>
+        <table class="table" border="1">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Age</th>
+            </tr>
+            <tr>
+                <td>2002060</td>
+                <td>Rajesh Biswas</td>
+                <td>22</td>
+            </tr>
+            <tr>
+                <td>2002061</td>
+                <td>Maynul Islam</td>
+                <td>23</td>
+            </tr>
+
+        </table>
+
+        <table class="table" border="1">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Age</th>
+            </tr>
+            <tr>
+                <td>2002060</td>
+                <td>Rajesh Biswas</td>
+                <td>22</td>
+            </tr>
+            <tr>
+                <td>2002061</td>
+                <td>Maynul Islam</td>
+                <td>23</td>
+            </tr>
+
+        </table>
+
+    </div>
+
+</body>
+
+</html>
